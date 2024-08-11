65aad5bffb4a1d3599eed2835ff6b477e6eaa5a2
Jeancarlo Ruano <jeancarloruano@Jeancarlos-MacBook-Pro.local>
2024-08-10 21:26:22 -0700

    added custom animation for scroll up button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -16,6 +17,19 @@ module.exports = {
         'inner-dark': 'inset 0 2px 4px rgba(24, 24, 27, 0.50)',
         'inner-dark-bezel': 'inset 0 4px 6px rgba(229, 231, 235, 0.20)',
       },
+      animationIterationCount: {
+        '1': '1',
+      },
+      animation: {
+        'bounce-fade': 'bounce-fade 0.75s ease-in-out',
+      },
+      keyframes: {
+        'bounce-fade': {
+          '0%, 50%, 100%': { transform: 'translateY(0)' },
+          '25%': { transform: 'translateY(-30px)' },
+          '75%': { transform: 'translateY(-15px)'},
+        },
+      },
     },
   },
   variants: {
@@ -23,6 +37,14 @@ module.exports = {
       boxShadow: ['dark'],
     },
   },
-  plugins: [],
+  plugins: [
+    function({ addUtilities }) {
+      addUtilities({
+        '.animation-iteration-1': {
+          animationIterationCount: '1',
+        },
+      })
+    }
+  ],
   darkMode: 'media',
 }
